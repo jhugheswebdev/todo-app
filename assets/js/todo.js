876a653538855a0c310888996f8d1ff43e31e4ce
James Hughes <heezmagnif@gmail.com>
2018-03-24 15:41:38 -0400

    Adds more styling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -17,6 +17,6 @@ $("input[type='text']").keypress(function() {
         var todoText = $(this).val();
         $(this).val("");
         //create new li and add to ul
-        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
     }
 });
