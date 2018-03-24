e1d363c58d73e14283df4ecdf10ae18185d3fcc2
James Hughes <heezmagnif@gmail.com>
2018-03-24 14:28:41 -0400

    Refactors jquery code for toggle

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,21 +1,4 @@
 // Check off specific dos by clicking
 $("li").click(function() {
-    // if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        // turn it black
-        $(this).css({
-            "color": "black",
-            "textDecoration": "none"
-        });
-
-        //else
-    } else {
-        //turn it grey
-        $(this).css({
-            "color": "gray",
-            "textDecoration": "line-through"
-        });
-    }
-
-
+    $(this).toggleClass("completed");
 });
